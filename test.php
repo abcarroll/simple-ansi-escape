aee91317dcba9c595461cf86e2b6367a30959a72
Ben Carroll <ben@plze.net>
2013-09-07 16:41:58 -0500

    Updated test.php to show 2-parameter syntax and for clarity.

--- test.php
+++ test.php
@@ -7,18 +7,23 @@
   * view the LICENSE file that was distributed with this source code.
   */
 
+
+
+
   include 'ansi_esc.php';
 
+
+  /* Every Map Possible.  See ansi_esc() for the list. */
+  echo "Every Map Possible, note that since some have multiple names (like positive and ~negative are equivilant), there are duplicates here:\n\n";
   foreach(SimpleAnsiEscape::$ansi_map as $k => $v) {
       echo str_pad($k, 15, ' ', STR_PAD_LEFT) . ': ';
-      echo ansi_esc($k) . 'a large piece of cake was eaten by me' . ansi_esc() . "\n";
+      echo ansi_esc($k) . 'a large piece of cake was eaten by me.' . ansi_esc() . "\n";
   }
-  
-  echo "\n----\n";
-  
-  echo ansi_esc('text/blue,bold,blink') . " ... And it was a " . ansi_esc('negative,~blink') . "great" . ansi_esc('~negative,blink') . " day\n" . ansi_esc();
-  
-  echo ansi_esc('text/pink') . "Many thanks to the Wikipedia Foundation & The Contributers to the Wikipedia ANSI Escape Codes Wiki Page" . ansi_esc();
-  
-  echo "\n----\n";
-  echo "This is an " . ansi_esc('uline') . 'incomplete' . ansi_esc('~uline') . " test.\n";
+
+  echo "\nDemonstration of changing, removing, and resetting formatting in the same line:\n";
+  echo ansi_esc('text/blue,bold,blink') . " ... And any day with cake is a " . ansi_esc('negative,~blink') . "great" . ansi_esc('~negative,blink') . " day!\n" . ansi_esc();
+  echo "----\n";
+
+  // Demonstration of one-call syntax
+  echo ansi_esc("bold, color/blue", "Simple ANSI Escape: http://github.com/nezzario/simple-ansi-escape") . "\n";
+  echo ansi_esc('text/pink', "Many thanks to the Wikipedia Foundation & The Contributers to the Wikipedia ANSI Escape Codes Wiki Page") . "\n";
