23290b32efde0c71b4c9d05ff23952566cd43575
A.B. Carroll <ben@hl9.net>
2015-01-21 00:15:40 -0500

    Code formatting changes.  No functional changes.

--- ansi_esc.php
+++ ansi_esc.php
@@ -1,8 +1,9 @@
 <?php
+
 /*
   * This file is a part of the simple-ansi-escape package.
   *
-  * Copyright (c) 2013, Armond B. Carroll <ben@hl9.net>
+  * Copyright (c) 2013-2015 Armond B. Carroll <ben@hl9.net>
   * Distributed under the BSD License.  For full license text, please
   * view the LICENSE file that was distributed with this source code.
   *
@@ -16,169 +17,170 @@
   */
 
 class SimpleAnsiEscape {
-  static public $ansi_map = [
-    'reset'                 =>  0, // Reset / Normal - all attributes off
-    'bold'                  =>  1, // Bold or increased intensity
-    'faint'                 =>  2, // Faint (decreased intensity) - not widely supported
-    'italic'                =>  3, // Italic: on - not widely supported. Sometimes treated as inverse.
-    'underline'             =>  4, // Underline: Single
+    static public $ansi_map = [
+        // Reset / Normal - all attributes off
+        'reset'        => 0,
+        // Bold or increased intensity
+        'bold'         => 1,
+        // Faint (decreased intensity) - not widely supported
+        'faint'        => 2,
+        // Italic: on - not widely supported. Sometimes treated as inverse.
+        'italic'       => 3,
+        // Underline: Single
+        'underline'    => 4,
+        // Blink: Slow - less than 150 per minute
+        'blink'        => 5,
+        'blinkslow'    => 5,
+        // Blink: Rapid - MS-DOS ANSI.SYS; 150 per minute or more; not widely supported
+        'blinkfast'    => 6,
+        // Image: Negative - inverse or reverse; swap foreground and background (reverse video)
+        'negative'     => 7,
+        '~positive'    => 7,
+        // Conceal - not widely supported
+        'conceal'      => 8,
+        '~reveal'      => 8,
+        // Crossed-out - Characters legible, but marked for deletion. Not widely supported.
+        'crossed'      => 9,
+        'font/default' => 10,
+        'font/0'       => 10,
+        // 11-19 - n-th alternate font
+        // Select the n-th alternate font. 14 being the fourth alternate font, up to 19 being the 9th alternate font.
+        'font/1'       => 11,
+        'font/2'       => 12,
+        'font/3'       => 13,
+        'font/4'       => 14,
+        'font/5'       => 15,
+        'font/6'       => 16,
+        'font/7'       => 17,
+        'font/8'       => 18,
+        'font/9'       => 19,
+        // Fraktur - Not widely supported
+        'fraktur'      => 20,
+        // Bold: off or Underline: Double - bold off not widely supported, double underline
+        // Didn't know exactly how to handle this as ~bold or 22 is more widely supported
+        'dblunderline' => 21,
+        '~~bold'       => 21,
+        // Normal color or intensity - neither bold nor faint, i.e. reset to normal
+        '~bold'        => 22,
+        '~faint'       => 22,
+        // Not italic. Not Fraktur
+        '~italic'      => 23,
+        '~fraktur'     => 23,
+        // Underline: None - not singly or doubly underlined
+        '~underline'   => 24,
+        // Blink: off
+        '~blink'       => 25,
+        // 26 -  Reserved
 
-    'blink'                 =>  5, // Blink: Slow - less than 150 per minute
-    'blinkslow'             =>  5, // Blink: Slow - less than 150 per minute
+        // Image: Positive
+        'positive'     => 27,
+        '~negative'    => 27,
+        // Reveal - conceal off
+        'reveal'       => 28,
+        '~conceal'     => 28,
+        // Not crossed out
+        '~crossed'     => 29,
+        // 30–37 - Set text color   30 + x, where x is from the color table below
+        // 0 Black 1 Red 2 Green 3 Yellow 4 Blue 5 Magent 6 Cyan 7 White
+        'text/black'   => 30,
+        'text/red'     => 31,
+        'text/green'   => 32,
+        'text/yellow'  => 33,
+        'text/blue'    => 34,
+        'text/magenta' => 35,
+        'text/cyan'    => 36,
+        'text/white'   => 37,
+        // Unsure how to handle these.
+        // 38 - Set xterm-256 text color[dubious – discuss] - next arguments are 5;x where x is color index (0..255)
+        'text/default' => 39, // Default text color - implementation defined (according to standard)
 
-    'blinkfast'             =>  6, // Blink: Rapid - MS-DOS ANSI.SYS; 150 per minute or more; not widely supported
+        // 40–47 - Set background color 40 + x, where x is from the color table below
+        // 0 Black 1 Red 2 Green 3 Yellow 4 Blue 5 Magent 6 Cyan 7 White
+        'bg/black'     => 40,
+        'bg/red'       => 41,
+        'bg/green'     => 42,
+        'bg/yellow'    => 43,
+        'bg/blue'      => 44,
+        'bg/magenta'   => 45,
+        'bg/cyan'      => 46,
+        'bg/white'     => 47,
+        // Unsure how to handle these
+        // 48 - Set xterm-256 background color - next arguments are 5;x where x is color index (0..255)
 
-    'negative'              =>  7, // Image: Negative - inverse or reverse; swap foreground and background (reverse video)
-    '~positive'             =>  7, // Image: Negative - inverse or reverse; swap foreground and background (reverse video)
+        // Default background color - implementation defined (according to standard)
+        'bg/default'   => 49,
+        // 50 - Reserved
 
-    'conceal'               =>  8, // Conceal - not widely supported
-    '~reveal'               =>  8, // Conceal - not widely supported
+        // Framed
+        'frame'        => 51,
+        // Encircled
+        'encircle'     => 52,
+        // Overlined
+        'overline'     => 53,
+        // Not framed or encircled
+        '~encircle'    => 54,
+        // Not framed or encircled
+        '~frame'       => 54,
+        // Not overlined
+        '~overline'    => 55,
+        // 56–59 - Reserved
 
-    'crossed'               =>  9, // Crossed-out - Characters legible, but marked for deletion. Not widely supported.
+        // Unsure how to handle these
+        // 60 - ideogram underline or right side line - hardly ever supported
+        // 61 - ideogram double underline or double line on the right side - hardly ever supported
+        // 62 - ideogram overline or left side line - hardly ever supported
+        // 63 - ideogram double overline or double line on the left side - hardly ever supported
+        // 64 - ideogram stress marking - hardly ever supported
+        // 90–99 - Set foreground color, high intensity - aixterm (not in standard)
+        // 100–109 - Set background color, high intensity - aixterm (not in standard)
 
-    'font/default'          => 10, // Primary(default) font
-    'font/0'                => 10, // Primary(default) font
-
-    // 11-19 - n-th alternate font  Select the n-th alternate font. 14 being the fourth alternate font, up to 19 being the 9th alternate font.
-    'font/1'                => 11,
-    'font/2'                => 12,
-    'font/3'                => 13,
-    'font/4'                => 14,
-    'font/5'                => 15,
-    'font/6'                => 16,
-    'font/7'                => 17,
-    'font/8'                => 18,
-    'font/9'                => 19,
-
-    'fraktur'               => 20, // Fraktur - hardly ever supported
-
-    'dblunderline'          => 21, // Bold: off or Underline: Double - bold off not widely supported, double underline hardly ever
-    // Didn't know exactly how to handle this as ~bold or 22 is more widely supported
-    '~~bold'                => 21, // Bold: off or Underline: Double - bold off not widely supported, double underline hardly ever
-
-    '~bold'                 => 22, // Normal color or intensity - neither bold nor faint
-    '~faint'                => 22, // Normal color or intensity - neither bold nor faint
-
-    '~italic'               => 23, // Not italic, not Fraktur
-    '~fraktur'              => 23, // Not italic, not Fraktur
-
-    '~underline'            => 24, // Underline: None - not singly or doubly underlined
-    '~blink'                => 25, // Blink: off
-
-    // 26 -  Reserved
-
-    'positive'          => 27, // Image: Positive
-    '~negative'         => 27, // Image: Positive
-
-    'reveal'            => 28, // Reveal - conceal off
-    '~conceal'          => 28, // Reveal - conceal off
-
-    '~crossed'          => 29, // Not crossed out
-
-    // 30–37 - Set text color   30 + x, where x is from the color table below
-    // 0 Black 1 Red 2 Green 3 Yellow 4 Blue 5 Magent 6 Cyan 7 White
-
-    'text/black'        => 30,
-    'text/red'          => 31,
-    'text/green'        => 32,
-    'text/yellow'       => 33,
-    'text/blue'         => 34,
-    'text/magenta'      => 35,
-    'text/cyan'         => 36,
-    'text/white'        => 37,
-
-    // Unsure how to handle these.
-    // 38 - Set xterm-256 text color[dubious – discuss] - next arguments are 5;x where x is color index (0..255)
-
-    'text/default'      => 39, // Default text color - implementation defined (according to standard)
-
-    // 40–47 - Set background color 40 + x, where x is from the color table below
-    // 0 Black 1 Red 2 Green 3 Yellow 4 Blue 5 Magent 6 Cyan 7 White
-    'bg/black'          => 40,
-    'bg/red'            => 41,
-    'bg/green'          => 42,
-    'bg/yellow'         => 43,
-    'bg/blue'           => 44,
-    'bg/magenta'        => 45,
-    'bg/cyan'           => 46,
-    'bg/white'          => 47,
-
-    // Unsure how to handle these
-    // 48 - Set xterm-256 background color - next arguments are 5;x where x is color index (0..255)
-
-    'bg/default'        => 49, // Default background color - implementation defined (according to standard)
-
-    // 50 - Reserved
-
-    'frame'             => 51, // Framed
-    'encircle'          => 52, // Encircled
-    'overline'          => 53, // Overlined
-
-    '~encircle'         => 54, // Not framed or encircled
-    '~frame'            => 54, // Not framed or encircled
-
-    '~overline'         => 55, // Not overlined
-
-    // 56–59 - Reserved
-
-    // Unsure how to handle these
-    // 60 - ideogram underline or right side line - hardly ever supported
-    // 61 - ideogram double underline or double line on the right side - hardly ever supported
-    // 62 - ideogram overline or left side line - hardly ever supported
-    // 63 - ideogram double overline or double line on the left side - hardly ever supported
-    // 64 - ideogram stress marking - hardly ever supported
-    // 90–99 - Set foreground color, high intensity - aixterm (not in standard)
-    // 100–109 - Set background color, high intensity - aixterm (not in standard)
-
-    // Stop ANSI Codes.
-  ];
+        // Stop ANSI Codes.
+    ];
 
 
-  // These are some trival shortcuts.  Note that the ',' => ';' replacement is hardcoded since it is only
-  // required when $format is passed as a string, and by doing so we avoid redundant calls to str_replace()
-  static public $preprocess_replace = [
-    '!' => '~', // Logical NOT style (so !bold is equiv to ~bold)
-    '^' => '~', // Regex NOT Style (so ^bold is equiv to ~bold)
+    // These are some trival shortcuts.  Note that the ',' => ';' replacement is hardcoded since it is only
+    // required when $format is passed as a string, and by doing so we avoid redundant calls to str_replace()
+    static public $preprocess_replace = [
+        '!'             => '~', // Logical NOT style (so !bold is equiv to ~bold)
+        '^'             => '~', // Regex NOT Style (so ^bold is equiv to ~bold)
 
-    'color' => 'text', // Very first time, I typed color/blue instead of text/blue
+        'color'         => 'text', // Very first time, I typed color/blue instead of text/blue
 
-    'strikethrough' => 'crossed',
-    'strikethru' => 'crossed',
+        'strikethrough' => 'crossed',
+        'strikethru'    => 'crossed',
+        'double'        => 'dbl', // Would anyone really want to type doubleunderline?
+        'uline'         => 'underline', // A shortcut that I might use
 
-    'double' => 'dbl', // Would anyone really want to type doubleunderline?
-    'uline' => 'underline', // A shortcut that I might use
+        // Did women come up with these ANSI color codes?  Because ain't no man I ever heard of referred to any
+        // color as "cyan" or "magenta".  That's light blue and f**cking pink.
+        'lightblue'     => 'cyan',
+        'pink'          => 'magenta',
+        // Too bad fraktur is rarely supported
+        'gothic'        => 'fraktur',
+    ];
 
-    // Did women come up with these ANSI color codes?  Because ain't no man I ever heard of referred to any
-    // color as "cyan" or "magenta".  That's light blue and f**cking pink.
-    'lightblue' => 'cyan',
-    'pink' => 'magenta',
+    static function AnsiEscape($format = 'reset', $wrap_around = '') {
+        $preprocess_replace_keys = array_keys(self::$preprocess_replace);
 
-    // Too bad fraktur is rarely supported
-    'gothic' => 'fraktur',
-  ];
+        // If it's not an array already, convert it to one
+        if(!is_array($format)) {
+            $format = str_replace(',', ';', $format); // Allows us to use comma as delimiter, see note above
+            $format = explode(';', $format);
+        }
 
-  static function AnsiEscape($format = 'reset', $wrap_around = '') {
-    $preprocess_replace_keys = array_keys(self::$preprocess_replace);
+        foreach($format as &$f) {
+            $f = str_replace($preprocess_replace_keys, self::$preprocess_replace, trim($f));
+            $f = self::$ansi_map[ $f ];
+        }
 
-    // If it's not an array already, convert it to one
-    if(!is_array($format)) {
-      $format = str_replace(',', ';', $format); // Allows us to use comma as delimiter, see note above
-      $format = explode(';', $format);
+        if(!empty($wrap_around)) {
+            $wrap_around .= self::AnsiEscape();
+        }
+
+        return "\033[" . implode(';', $format) . "m$wrap_around";
     }
-
-    foreach($format as &$f) {
-      $f = str_replace($preprocess_replace_keys, self::$preprocess_replace, trim($f));
-      $f = self::$ansi_map[$f];
-    }
-
-    if(!empty($wrap_around)) {
-      $wrap_around .= self::AnsiEscape();
-    }
-
-    return "\033[" . implode(';', $format) . "m$wrap_around";
-  }
 }
 
 function ansi_esc($format = 'reset', $wrap_around = '') {
-  return SimpleAnsiEscape::AnsiEscape($format, $wrap_around);
+    return SimpleAnsiEscape::AnsiEscape($format, $wrap_around);
 }
--- test.php
+++ test.php
@@ -1,29 +1,25 @@
 <?php
- /*
-  * This file is a part of the simple-ansi-escape package.
-  *
-  * Copyright (c) 2013, Armond B. Carroll <ben@hl9.net>
-  * Distributed under the BSD License.  For full license text, please
-  * view the LICENSE file that was distributed with this source code.
-  */
+/*
+ * This file is a part of the simple-ansi-escape package.
+ *
+ * Copyright (c) 2013, Armond B. Carroll <ben@hl9.net>
+ * Distributed under the BSD License.  For full license text, please
+ * view the LICENSE file that was distributed with this source code.
+ */
 
+include 'ansi_esc.php';
 
+/* Every Map Possible.  See ansi_esc() for the list. */
+echo "Every Map Possible, note that since some have multiple names (like positive and ~negative are equivilant), there are duplicates here:\n\n";
+foreach(SimpleAnsiEscape::$ansi_map as $k => $v) {
+    echo str_pad($k, 15, ' ', STR_PAD_LEFT) . ': ';
+    echo ansi_esc($k) . 'a large piece of cake was eaten by me.' . ansi_esc() . "\n";
+}
 
+echo "\nDemonstration of changing, removing, and resetting formatting in the same line:\n";
+echo ansi_esc('text/blue,bold,blink') . " ... And any day with cake is a " . ansi_esc('negative,~blink') . "great" . ansi_esc('~negative,blink') . " day!\n" . ansi_esc();
+echo "----\n";
 
-  include 'ansi_esc.php';
-
-
-  /* Every Map Possible.  See ansi_esc() for the list. */
-  echo "Every Map Possible, note that since some have multiple names (like positive and ~negative are equivilant), there are duplicates here:\n\n";
-  foreach(SimpleAnsiEscape::$ansi_map as $k => $v) {
-      echo str_pad($k, 15, ' ', STR_PAD_LEFT) . ': ';
-      echo ansi_esc($k) . 'a large piece of cake was eaten by me.' . ansi_esc() . "\n";
-  }
-
-  echo "\nDemonstration of changing, removing, and resetting formatting in the same line:\n";
-  echo ansi_esc('text/blue,bold,blink') . " ... And any day with cake is a " . ansi_esc('negative,~blink') . "great" . ansi_esc('~negative,blink') . " day!\n" . ansi_esc();
-  echo "----\n";
-
-  // Demonstration of one-call syntax
-  echo ansi_esc("bold, color/blue", "Simple ANSI Escape: http://github.com/nezzario/simple-ansi-escape") . "\n";
-  echo ansi_esc('text/pink', "Many thanks to the Wikipedia Foundation & The Contributers to the Wikipedia ANSI Escape Codes Wiki Page") . "\n";
+// Demonstration of one-call syntax
+echo ansi_esc("bold, color/blue", "Simple ANSI Escape: http://github.com/nezzario/simple-ansi-escape") . "\n";
+echo ansi_esc('text/pink', "Many thanks to the Wikipedia Foundation & The Contributers to the Wikipedia ANSI Escape Codes Wiki Page") . "\n";
